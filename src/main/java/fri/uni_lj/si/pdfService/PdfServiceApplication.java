0432a5bcae0a0ec2d3b880ba456af8ed6cb4f869
zigakleine <ziga.kleine@gmail.com>
2020-12-25 16:47:19 +0100

    Added basic spring boot app.

---
+++ src/main/java/fri/uni_lj/si/pdfService/PdfServiceApplication.java
@@ -0,0 +1,13 @@
+package fri.uni_lj.si.pdfService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfServiceApplication.class, args);
+	}
+
+}
